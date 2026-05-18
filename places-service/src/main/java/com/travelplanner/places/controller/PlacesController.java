8e52d9ac8528034c0b49b46de3734e10b532b546
shabunovasofia <shabunova.s@gmail.com>
2026-05-18 23:19:27 +0300

    добавление @Valid для включения валидации запроса

--- places-service/src/main/java/com/travelplanner/places/controller/PlacesController.java
+++ places-service/src/main/java/com/travelplanner/places/controller/PlacesController.java
@@ -3,6 +3,7 @@ package com.travelplanner.places.controller;
 import com.travelplanner.places.dto.PlacesSearchRequest;
 import com.travelplanner.places.dto.PlacesSearchResponse;
 import com.travelplanner.places.service.PlacesService;
+import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,7 +20,7 @@ public class PlacesController {
   }
 
   @PostMapping("/search")
-  public PlacesSearchResponse searchResponse(@RequestBody PlacesSearchRequest request) {
+  public PlacesSearchResponse searchResponse(@Valid @RequestBody PlacesSearchRequest request) {
     return placesService.search(request);
   }
 }
